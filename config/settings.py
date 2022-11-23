10ce5cbabc80bf9c1bcf363169829a87fa5b9fd2
nazmulhasan7447 <nazmulhasan747406@gmail.com>
2022-11-24 03:12:39 +0600

    added trusted origin

--- config/settings.py
+++ config/settings.py
@@ -159,6 +159,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:3000",
+    "https://dolphin-app-a8u55.ondigitalocean.app"
 ]
 
 CSRF_COOKIE_SAMESITE = 'Lax'
@@ -173,10 +174,12 @@ SESSION_COOKIE_HTTPONLY = False
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'https://dolphin-app-a8u55.ondigitalocean.app'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
+    'https://dolphin-app-a8u55.ondigitalocean.app'
 ]
 
 CORS_EXPOSE_HEADERS = ['Content-Type', 'X-CSRFToken']
